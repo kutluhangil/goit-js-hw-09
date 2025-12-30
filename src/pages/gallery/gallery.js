e89f91310a8de0b03ccfed439b90dcd80b3a87c4
kutluhangil <kutluhangul@windowslive.com>
2025-12-31 01:09:56 +0300

    Fix gallery simplelightbox module issue and rebuild

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -2,18 +2,11 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import images from '../../data.js';
 
-console.log('Gallery script loaded');
-console.log('Images data:', images);
-
 const galleryContainer = document.querySelector('.gallery');
-console.log('Gallery container:', galleryContainer);
 
-if (!galleryContainer) {
-  console.error('Gallery container not found!');
-} else {
-  const markup = images
-    .map(
-      ({ preview, original, description }) => `
+const markup = images
+  .map(
+    ({ preview, original, description }) => `
     <li class="gallery-item">
       <a class="gallery-link" href="${original}">
         <img
@@ -23,16 +16,12 @@ if (!galleryContainer) {
         />
       </a>
     </li>`
-    )
-    .join('');
+  )
+  .join('');
 
-  console.log('Markup created, length:', markup.length);
-  galleryContainer.insertAdjacentHTML('beforeend', markup);
-  console.log('Markup inserted into DOM');
+galleryContainer.insertAdjacentHTML('beforeend', markup);
 
-  new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
-  });
-  console.log('SimpleLightbox initialized');
-}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
